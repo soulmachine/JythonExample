90234046544e4d1cdc81f05b8d54a36b46c03dc1
Frank <soulmachine@gmail.com>
2015-05-05 17:33:49 -0700

    add an example: define a Python function then call it from Java

--- JythonExample/src/main/java/me/soulmachine/JythonExample.java
+++ JythonExample/src/main/java/me/soulmachine/JythonExample.java
@@ -1,6 +1,7 @@
 package me.soulmachine;
 
 import org.python.core.PyObject;
+import org.python.core.PyString;
 import org.python.util.PythonInterpreter;
 
 import java.util.List;
@@ -26,20 +27,30 @@ final class JythonExample {
   public static void main(final String[] args) throws ScriptException {
     listEngines();
 
-    final ScriptEngineManager mgr = new ScriptEngineManager();
-    final ScriptEngine pyEngine = mgr.getEngineByName("python");
+    // Integrate with ScriptEngine
+    {
+      final ScriptEngineManager mgr = new ScriptEngineManager();
+      final ScriptEngine pyEngine = mgr.getEngineByName("python");
 
-    try {
-      pyEngine.eval("print \"Python - Hello, world!\"");
-    } catch (ScriptException ex) {
-      ex.printStackTrace();
+      try {
+        pyEngine.eval("print \"Python - Hello, world!\"");
+      } catch (ScriptException ex) {
+        ex.printStackTrace();
+      }
     }
 
     final PythonInterpreter interpreter = new PythonInterpreter();
     interpreter.exec("print \"Python - Hello, world!\"");
 
-    final PyObject result = interpreter.eval("2 + 3");
+    PyObject result = interpreter.eval("2 + 3");
     System.out.println(result.toString());
+
+    // Define a function and call it from Java
+    interpreter.exec("def myLowerCase(s):\n\treturn s.lower()\n");
+    final PyObject myLowerCase = interpreter.get("myLowerCase");
+    result = myLowerCase.__call__(new PyString("TEST!"));
+    final String realResult = (String)result.__tojava__(String.class);
+    System.out.println(realResult);
   }
 
   /**
